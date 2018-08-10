f07e73f50d06d962c37756912a3d35ac381fb04b
thextool <matthew.riach@gmail.com>
2018-08-10 13:03:50 -0600

    :zap: added the header and brochure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header'
+import Brochure from './Components/Brochure'
 
 class App extends Component {
 
@@ -16,20 +17,7 @@ class App extends Component {
             <body>
                 <Header />
                 <main>
-                    <section>
-                        <h2 className="welcome">Welcome to Q-Pops! A Matt Riach creation! Play awhile!</h2>
-                        <h4 className="lead intro">In social situation when someone quotes a tv show 
-                            and you feel like an idiot because you don’t know
-                            where it comes from or other funny and cool
-                            quotes to respond with. Studies have shown the 
-                            2 most quoted shows in all social situations 
-                            are Ron Swanson from Parks and Recreation and 
-                            Breaking Bad. Now your knowledge and learn some 
-                            new quotes. So the next time you are in a social 
-                            situation you will have to knowledge to be the 
-                            coolest kid on the block!
-                        </h4>
-                    </section>
+                    <Brochure />
                     <section className="quoteAndButton">
                         <button type="button" className="btn btn-primary btn-lg newQuote">New Quote</button>
                         <h3>Guess the Quote:</h3>
---
+++ src/Components/Brochure.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Brocher = () => {
+    return(
+        <section>
+            <h2 className="welcome">Welcome to Q-Pops! A Matt Riach creation! Play awhile!</h2>
+            <h4 className="lead intro">In social situation when someone quotes a tv show 
+                and you feel like an idiot because you don’t know
+                where it comes from or other funny and cool
+                quotes to respond with. Studies have shown the 
+                2 most quoted shows in all social situations 
+                are Ron Swanson from Parks and Recreation and 
+                Breaking Bad. Now your knowledge and learn some 
+                new quotes. So the next time you are in a social 
+                situation you will have to knowledge to be the 
+                coolest kid on the block!
+            </h4>
+        </section>
+    )
+}
+
+export default Brocher
